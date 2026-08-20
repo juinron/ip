99eaa11708f16bee5b6e9027d23545d57545cf4d
juinron <juinron04@gmail.com>
2026-08-20 15:49:41 +0800

    Add list

--- src/main/java/Aider.java
+++ src/main/java/Aider.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
  * Entry point for the Aider chatbot application.
  */
 public class Aider {
+    /** Maximum number of tasks that can be stored in memory. */
+    private static final int MAX_TASKS = 100;
+
     /** Separates the chatbot's responses in the console. */
     private static final String SEPARATOR = "____________________________________________________________";
 
@@ -21,6 +24,8 @@ public class Aider {
         System.out.println("What can I do for you?");
         System.out.println(SEPARATOR);
 
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -32,7 +37,18 @@ public class Aider {
                 break;
             }
 
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
+            } else {
+                System.out.println("Task limit reached.");
+            }
+
             System.out.println(SEPARATOR);
         }
     }
