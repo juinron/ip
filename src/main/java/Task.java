ea47054e364bd4c59775dfab69240102ef5ae8ef
juinron <juinron04@gmail.com>
2026-08-20 16:05:30 +0800

    Add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,48 @@
+/**
+ * Represents a task entered by the user.
+ */
+public class Task {
+    /** The text describing the task. */
+    protected String description;
+
+    /** Whether the task has been completed. */
+    protected boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description the text describing the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /** Marks this task as completed. */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /** Marks this task as incomplete. */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns the symbol used to display this task's status.
+     *
+     * @return {@code X} when done, or a space when not done
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Returns the task description.
+     *
+     * @return the task description
+     */
+    public String getDescription() {
+        return description;
+    }
+}
